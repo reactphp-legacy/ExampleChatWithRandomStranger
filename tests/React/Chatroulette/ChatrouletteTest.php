0bc2dbde2357201e1a05ed0b2cf5a531ab56bbfd
Igor Wiedler <igor@wiedler.ch>
2012-10-13 03:27:25 +0200

    Add conversation between alice and bob to tests

--- tests/React/Chatroulette/ChatrouletteTest.php
+++ tests/React/Chatroulette/ChatrouletteTest.php
@@ -4,6 +4,7 @@ namespace React\Chatroulette;
 
 use Monolog\Logger;
 use Monolog\Handler\TestHandler;
+use React\Socket\ConnectionInterface;
 
 class ChatrouletteTest extends \PHPUnit_Framework_TestCase
 {
@@ -46,15 +47,20 @@ class ChatrouletteTest extends \PHPUnit_Framework_TestCase
         $this->chatroulette->connect($alice);
         $this->chatroulette->connect($bob);
 
+        $this->emitConnectionData($alice, 'Hallo Bob, wie geht es dir?');
+        $this->emitConnectionData($bob, 'Je ne comprends pas!');
+
         $this->assertConnectionData($alice, [
             'Hello Alice!',
             'Please wait until a partner connects.',
             'You are now talking to Bob.',
+            'Je ne comprends pas!',
         ]);
 
         $this->assertConnectionData($bob, [
             'Hello Bob!',
             'You are now talking to Alice.',
+            'Hallo Bob, wie geht es dir?',
         ]);
 
         $this->assertLogs([
@@ -103,13 +109,18 @@ class ChatrouletteTest extends \PHPUnit_Framework_TestCase
         return array($alice, $bob);
     }
 
-    private function assertClosed($conn)
+    private function emitConnectionData(ConnectionInterface $conn, $data)
+    {
+        $conn->emit('data', array($data."\n"));
+    }
+
+    private function assertClosed(ConnectionInterface $conn)
     {
         $this->assertFalse($conn->isReadable());
         $this->assertFalse($conn->isWritable());
     }
 
-    private function assertConnectionData($conn, $expected)
+    private function assertConnectionData(ConnectionInterface $conn, $expected)
     {
         $data = implode("\n", $expected)."\n";
         $this->assertSame($data, $conn->data);
